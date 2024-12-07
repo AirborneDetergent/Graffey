996fea58e482a850c5c425eebab3d27b2b237098
Joshua-Adler <thisemail1579@gmail.com>
2024-12-06 21:56:52 -0800

    pan and zoom controls

--- graph.js
+++ graph.js
@@ -20,7 +20,6 @@ class Display {
 	
 	updateCanvasSize(entry) {
 		let oldWidth = this.width;
-		let oldHeight = this.height;
 		this.width = entry.devicePixelContentBoxSize[0].inlineSize;
 		this.height = entry.devicePixelContentBoxSize[0].blockSize;
 		if(this.hasResized) {
@@ -29,7 +28,6 @@ class Display {
 			// Handle initial resizing by assuming the aspect ratio was 1:1 before
 			this.camera.updateResized(this.height, this.width);
 		}
-		console.log(this.canvas.width, this.canvas.height);
 		this.hasResized = true;
 	}
 	
@@ -53,7 +51,8 @@ class Display {
 	
 	drawGrid(granularity) {
 		this.ctx.beginPath();
-		let scale = this.camera.maxX - this.camera.minX;
+		let aspect = this.width / this.height;
+		let scale = (this.camera.maxX - this.camera.minX) / aspect;
 		scale = 2 ** Math.ceil(Math.log2(scale) - granularity);
 		for(let i = Math.round(this.camera.minX / scale) * scale; i <= Math.round(this.camera.maxX / scale) * scale; i+= scale) {
 			this.ctx.moveTo(this.camera.ssx(i), 0);
@@ -76,6 +75,50 @@ class Camera {
 		this.minY = -10;
 		this.maxX = 10;
 		this.maxY = 10;
+		this.isDragging = false;
+		this.oldMX = 0;
+		this.oldMY = 0;
+		this.display.canvas.addEventListener('pointerdown', this.handleDown.bind(this));
+		window.addEventListener('pointerup', this.handleUp.bind(this));
+		window.addEventListener('pointermove', this.handleDrag.bind(this));
+		this.display.canvas.addEventListener('wheel', this.handleWheel.bind(this));
+	}
+	
+	handleUp() {
+		this.isDragging = false;
+	}
+	
+	handleDown(event) { 
+		this.oldMX = event.x;
+		this.oldMY = event.y;
+		this.isDragging = true;
+	}
+	
+	handleDrag(event) {
+		if(this.isDragging) {
+			let dx = (this.oldMX - event.x) * window.devicePixelRatio / this.display.width;
+			let dy = -((this.oldMY - event.y) * window.devicePixelRatio / this.display.height);
+			let sx = this.maxX - this.minX;
+			let sy = this.maxY - this.minY;
+			this.minX += dx * sx;
+			this.maxX += dx * sx;
+			this.minY += dy * sy;
+			this.maxY += dy * sy;
+			this.oldMX = event.x;
+			this.oldMY = event.y;
+		}
+	}
+	
+	handleWheel(event) {
+		let ratio = 1.1 ** (event.deltaY / 100);
+		let mx = (event.x - event.originalTarget.offsetLeft) * window.devicePixelRatio / this.display.width;
+		let my = 1 -  (event.y - event.originalTarget.offsetTop) * window.devicePixelRatio / this.display.height;
+		mx = mx * (this.maxX - this.minX) + this.minX;
+		my = my * (this.maxY - this.minY) + this.minY;
+		this.minX = (this.minX - mx) * ratio + mx;
+		this.maxX = (this.maxX - mx) * ratio + mx;
+		this.minY = (this.minY - my) * ratio + my;
+		this.maxY = (this.maxY - my) * ratio + my;
 	}
 	
 	updateResized(oldWidth, width) {
@@ -94,6 +137,6 @@ class Camera {
 	}
 	
 	ssy(y) {
-		return Math.round(this.ssRemap(this.display.height, this.minY, this.maxY, y));
+		return this.display.height - Math.round(this.ssRemap(this.display.height, this.minY, this.maxY, y));
 	}
 }
